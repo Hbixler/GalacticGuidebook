8ddafb26653d5f7bbdc58d3cb9a22956d602bb55
Hailey Bixler <hbixler@unomaha.edu>
2020-06-29 08:02:01 -0700

    Hi

---
+++ Galactic Guidebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Galactic Guidebook
+//
+//  Created by Hailey Bixler on 6/29/20.
+//  Copyright © 2020 Hailey Bixler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
